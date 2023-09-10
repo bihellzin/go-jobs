ea571de4a30cab0d0b7585ac29d61306a9eadd8d
bihellzin <oliva.gabriel@outlook.com>
2023-09-10 20:43:27 -0300

    feat: add OpeningResponse struct

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,6 +1,10 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Opening struct {
 	gorm.Model
@@ -9,4 +13,16 @@ type Opening struct {
 	Remote bool
 	Link string
 	Salary int64
+}
+
+type OpeningResponse struct {
+	Id 				uint 			`json:"id"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt,omitempty"`
+	Role 			string 		`json:"role"`
+	Company 	string 		`json:"company"`
+	Remote 		bool 			`json:"remote"`
+	Link 			string 		`json:"link"`
+	Salary 		int64 		`json:"salary"`
 }
