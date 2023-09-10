f2117f5f6e4ebb6077bb296a881e03b7ec2ac7ff
bihellzin <oliva.gabriel@outlook.com>
2023-09-10 18:50:53 -0300

    adds router package

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/bihellzin/go-jobs/router"
+)
 
 func main()  {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	router.Initialize()
 }
