d2a32766492a0f54e7688922886c8e364b937731
bihellzin <oliva.gabriel@outlook.com>
2023-09-10 20:15:39 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/bihellzin/go-jobs/config"
 	"github.com/bihellzin/go-jobs/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main()  {
+	logger := config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
